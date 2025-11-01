fdb312d10079d032326c1d059415b7993797b399
atiwari@perinimble.com <atiwari@perinimble.com>
2025-11-02 00:57:40 +0530

    all commands invalid

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,13 @@
 
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         System.out.print("$ ");
+        Scanner scannerObj = new Scanner(System.in);
+        String input = scannerObj.nextLine();
+        System.out.println(input + ": command not found");
     }
 }
