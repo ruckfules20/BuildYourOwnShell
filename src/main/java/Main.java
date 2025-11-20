6582bece41fbbb88068281645581691604efa3a8
atiwari@perinimble.com <atiwari@perinimble.com>
2025-11-20 23:06:22 +0530

    echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,18 +5,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
+        
+        
+
+
         boolean x=false;
         while(!x){
-                 System.out.print("$ ");
+             
+        System.out.print("$ ");
         Scanner scannerObj = new Scanner(System.in);
         String input = scannerObj.nextLine();
-
+        input =input.strip();
+        if(input.startsWith("echo")){
+            // System.out.println(input);
+            input =input.replace("echo","");
+            input.strip();
+            System.out.println(input);
+        }
         // TODO: make this a regex later
-        if(input.equalsIgnoreCase("exit 0")){
+        else if(input.equalsIgnoreCase("exit 0")){
             x=true;
             break;
         }
-        System.out.println(input + ": command not found");   
+        else{System.out.println(input + ": command not found");  } 
         }
   
         
